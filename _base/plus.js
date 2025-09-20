74a7a09e5457e8163d476a1b8b4a29e2a20b272e
VSimon <vita.simon@centrum.cz>
2025-09-20 14:27:07 +0200

    BASE : added watermark to TOC tab

--- _base/plus.js
+++ _base/plus.js
@@ -1,3 +1,6 @@
 const customizationPluginName = 'puiButtonCustPackage';
 loadPlugin(customizationPluginName, loadPluginListBasePath(customizationPluginName)).then(x => 
   activatePlugin(customizationPluginName, '', STO_DATA));
+const pluginWatermark = 'puiWatermark';
+deactivatePlugin(pluginWatermark);
+activatePlugin(pluginWatermark, '', source = STO_HELP);
