da62d3a5414985819f1614e806d1fc937edae587
VSimon <vita.simon@centrum.cz>
2025-09-20 14:41:50 +0200

    BASE : watermark deactivation removed, deactivated by default

--- _base/plus.js
+++ _base/plus.js
@@ -2,5 +2,4 @@ const customizationPluginName = 'puiButtonCustPackage';
 loadPlugin(customizationPluginName, loadPluginListBasePath(customizationPluginName)).then(x => 
   activatePlugin(customizationPluginName, '', STO_DATA));
 const pluginWatermark = 'puiWatermark';
-deactivatePlugin(pluginWatermark);
 activatePlugin(pluginWatermark, '', source = STO_HELP);
